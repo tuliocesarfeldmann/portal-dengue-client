a292e12433407a5f3d874e1fee9ccbc5a9716717
Michel F. K. Forsch <pessoal@michelfkf.dev.br>
2024-11-25 21:26:54 -0300

    Fix backend URL for API calls

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://200.132.38.218:8887'
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://michelfkf.dev.br:8887'
